50174308fafd380968f129d4652cad62af8e52b0
Mirza <sayheymirza@gmail.com>
2025-10-24 23:09:29 +0330

    🔨 build error fixed

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,16 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import serviceDynamicJson from './routes/service/service-dynamic.json';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'service/:category',
+    getPrerenderParams: async () => {
+      return Object.keys(serviceDynamicJson).map(category => ({ category }));
+    },
     renderMode: RenderMode.Prerender
   }
 ];
